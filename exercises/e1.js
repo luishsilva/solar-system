bdfb87fbdc37af841d87a48f5f76682454f33cd4
luishsilva <luizhrick@gmail.com>
2023-03-23 08:45:10 -0700

    Remove unnecessary comment to test change name repository push

--- exercises/e1.js
+++ exercises/e1.js
@@ -5,7 +5,6 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetNames(data) {
-  // Your code goes here...
   let planetNames = data.planets.map(({ name }) => name)
   return planetNames
 }
